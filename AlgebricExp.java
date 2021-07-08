a3e70ce0b764aaa4223398c4a1854e8d66d75439
ddgvv <33596297+ddgvv@users.noreply.github.com>
2021-07-08 13:09:17 +0530

    Update AlgebricExp.java

--- AlgebricExp.java
+++ AlgebricExp.java
@@ -1,11 +1,31 @@
-import java.util.Arrays;
+package it;
 
 public class AlgebricExp {
 
+	static void removeDuplicate(AlgebricExpresssion algebricExpresssionTemp) {
+		AlgebricExpresssion algebricExpresssion = new AlgebricExpresssion();
+		algebricExpresssion.expresssion[0] =  algebricExpresssionTemp.expresssion[0];
+		algebricExpresssion.expresssionLength =  1;
+		for (int m = 1; m < algebricExpresssionTemp.expresssionLength; m++) {
+			int visited = 0;
+			for (int n = 0; n < algebricExpresssion.expresssionLength; n++) {
+				if(algebricExpresssionTemp.expresssion[m].expression.equals(algebricExpresssion.expresssion[n].expression)) {
+					algebricExpresssion.expresssion[n].coEff += algebricExpresssionTemp.expresssion[m].coEff;
+					visited = 1; break;
+				}
+			}
+			if(visited == 0) {
+				algebricExpresssion.expresssion[algebricExpresssion.expresssionLength++] = algebricExpresssionTemp.expresssion[m];
+			}
+		}
+		System.out.println(algebricExpresssion);
+	}
+
+	
 	static Expresssion orderVariablesByAsciiCode(Expresssion expresssion) {
 		for (int m = 0; m < expresssion.variableLength; m++) {
 			for (int n = 0; n < expresssion.variableLength; n++) {
-				if(expresssion.variablePower[m].ascii > expresssion.variablePower[n].ascii){
+				if(expresssion.variablePower[m].ascii < expresssion.variablePower[n].ascii){
 					Variable variableTemp = new Variable();
 					variableTemp = expresssion.variablePower[m];
 					expresssion.variablePower[m] = expresssion.variablePower[n];
@@ -13,20 +33,20 @@ public class AlgebricExp {
 				}
 			}
 		}
-		System.out.println(expresssion);
+		expresssion.toString();
 		return expresssion;
 	}
+	
 	static void doExpressionMultiply(AlgebricExpresssion[] algebricExpresssion,int algebricLength) {
+		AlgebricExpresssion algebricExpresssionTemp = new AlgebricExpresssion();
 		for (int i = 1; i < algebricLength; i++) {
 			AlgebricExpresssion algebricExpresssionPrev = algebricExpresssion[i-1];
 			AlgebricExpresssion algebricExpresssionCur = algebricExpresssion[i];
-			System.out.println(algebricExpresssionCur );
-			System.out.println(algebricExpresssionPrev);
+			int tempExpLength = 0;
 			for (int j = 0; j < algebricExpresssionPrev.expresssionLength; j++) {
 				for (int k = 0; k < algebricExpresssionCur.expresssionLength; k++) {
 					Expresssion expresssion = new Expresssion();
 					expresssion.coEff = algebricExpresssionCur.expresssion[k].coEff * algebricExpresssionPrev.expresssion[j].coEff;
-					
 					for (int l = 0; l < algebricExpresssionCur.expresssion[k].variableLength; l++) {
 						Variable variable = new Variable();
 						variable.power = algebricExpresssionCur.expresssion[k].variablePower[l].power;
@@ -35,12 +55,12 @@ public class AlgebricExp {
 						expresssion.variablePower[l] = variable;
 						expresssion.variableLength = algebricExpresssionCur.expresssion[k].variableLength;
 					}
-					
 					int l = 0;
 					while(l < algebricExpresssionPrev.expresssion[j].variableLength) {
 						Variable variable = algebricExpresssionPrev.expresssion[j].variablePower[l];
 						int visited = 0;
 						int firstArrLen = expresssion.variableLength;
+						
 						for (int m = 0; m < algebricExpresssionCur.expresssion[k].variableLength; m++) {
 							Variable variable2 = expresssion.variablePower[m];
 							if(variable.var.equals(variable2.var)) {
@@ -51,15 +71,18 @@ public class AlgebricExp {
 						if (visited == 0) {
 							expresssion.variablePower[firstArrLen++] = variable;
 							expresssion.variableLength = firstArrLen;
-							expresssion.variablePower[firstArrLen].ascii = variable.var.charAt(0) - 0;
+							expresssion.variablePower[firstArrLen-1].ascii = variable.var.charAt(0) - 0;
 						}
 						l++;
 					}
 					expresssion = orderVariablesByAsciiCode(expresssion);
-					System.out.println(expresssion);
+					algebricExpresssionTemp.expresssion[tempExpLength++] = expresssion;
+					algebricExpresssionTemp.expresssionLength = tempExpLength;
 				}
+				algebricExpresssion[i] = algebricExpresssionTemp;
 			}
 		}
+		removeDuplicate(algebricExpresssionTemp);
 	}
 	
 	static void getExpressionArray(String input) {
@@ -88,7 +111,11 @@ public class AlgebricExp {
 	}
 }
 
+//+10x^3y^2z +6x^2y^3z -4x^2yz +15x^2y^3z^2 +9xy^4z^2 -6xy^2z^2 -5x^2yz^4 -3xy^2z^4 +2xz^4
+//10x3y2z    +6x2y3z   -4x2yz  +15x2y3z2    +9xy4z2 -6xy2z2 -5x2yz4 -3xy2z4 +2xz4
+//+10x3y2z   +6x2y3z   -4x2yz  +15x2y3z2    +9xy4z2 -6xy2z2 -5x2yz4 -3xy2z4 +2xz4
 class AlgebricExpresssion {
+	
 	Expresssion[] expresssion = new Expresssion[10];
 	int expresssionLength;
 	
@@ -98,8 +125,9 @@ class AlgebricExpresssion {
 		for(int i = 0 ; i < expresssionLength; i++) {
 			res += expresssion[i].toString();
 		}
-		return res + "  -->length is " + expresssionLength;
+		return res;
 	}
+	
 }
 
 class Expresssion {
@@ -115,11 +143,15 @@ class Expresssion {
 		for(int i = 0 ; i < variableLength; i++) {
 			res += variablePower[i].toString();
 		}
-		return " " + coEff + res;
+		expression = res;
+		String coEffString = coEff<0 ? coEff+"" : "+" + coEff;
+		return " "+coEffString + "" + expression;
 	}
+	
 	Expresssion(){
 		
 	}
+	
 	Expresssion(String val) {
 		val = val.replace("=", "-");
 		int sign = val.charAt(0) == '-' ? -1 : 1 ;
@@ -142,6 +174,7 @@ class Expresssion {
 		}
 		variableLength = varLength;
  	}
+	
 	private int getDigitEnd(String val) {
 		int endIndex = 0;
 		for (int i = 0; i < val.length() ; i++) {
@@ -154,7 +187,6 @@ class Expresssion {
 		}
 		return endIndex;
 	}
-	
 }
 
 class Variable {
@@ -164,6 +196,6 @@ class Variable {
 	@Override
 	public String toString() {
 		String pow = power > 1 ? "^"+power : "";
-		return var + "(" + ascii + ")" + pow;
+		return var + pow;
 	}
 }
